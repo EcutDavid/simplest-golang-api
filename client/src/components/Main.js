3fcd145f3419e06f6e604b6f8b7c4f3cfddd8318
David Guan <davidguandev@gmail.com>
2016-06-30 21:52:33 +0800

    Correct typo.

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -35,7 +35,7 @@ class AppComponent extends React.Component {
           count ? (
             <div>
               <h1>Thanks for visit</h1>
-              <h2>This webpage has been visted { `${count} ${count > 1 ? 'times' : 'time'}` }</h2>
+              <h2>This webpage has been visited { `${count} ${count > 1 ? 'times' : 'time'}` }</h2>
               <h2>Your IP address is: {ip.replace(/:[0-9]{1,}/g, '')}</h2>
             </div>
           ) : (
